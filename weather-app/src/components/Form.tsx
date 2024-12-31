d53cdba51b3c4b31ab502d47d78b02f81e839ff1
yuki-tsurut <nfcgt.30@gmail.com>
2024-12-31 23:21:46 +0900

    適当にAPI追加

--- weather-app/src/components/Form.tsx
+++ weather-app/src/components/Form.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 const Form = () => {
+    const [city, setCity] = useState<string>("")
+
+    const getWeather = (e: any) => {
+        e.preventDefault()
+        fetch("http://api.weatherapi.com/v1/current.json?key=34ac5d97dacb4141901134526243112&q=London&aqi=no")
+         .then(res => res.json())
+         .then(data => console.log(data))
+    }
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" />
-            <button type="submit">Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value)} />
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     )
 }
