0b7f20ee9ec43a2faac44e2d776f6baaad044151
yuki-tsurut <nfcgt.30@gmail.com>
2025-01-02 22:45:50 +0900

    ナビゲーション追加

--- mercari-clone/src/App.tsx
+++ mercari-clone/src/App.tsx
@@ -1,29 +1,10 @@
-import { useState } from 'react';
-import { TodoList } from './components/TodoList';
-import { dummyTodoList } from './data/dummyTodoList';
+import { PageNavigation } from "./layouts"
 
-function App() {
-  const [todoList, setTodoList] = useState(dummyTodoList);
-
-  const changeCompleted = (id: number) => {
-    setTodoList((prevTodoList) => {
-      return prevTodoList.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      });
-    });
-  };
-
+export default function App() {
   return (
-    <main className="mx-auto mt-10 max-w-xl space-y-10">
-      <h1 className="text-center text-4xl">Todoアプリ</h1>
-      <div className="rounded bg-slate-200 p-5">
-        <TodoList todoList={todoList} changeCompleted={changeCompleted} />
-      </div>
-    </main>
-  );
+    <div>
+      <div className="min-h-screen"></div>
+      <PageNavigation />
+    </div>
+  )
 }
-
-export default App;
