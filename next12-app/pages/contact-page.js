d0f93744e68b89e4d241ce8cf1282fc0c013f35a
yuki-tsurut <nfcgt.30@gmail.com>
2025-01-06 01:05:05 +0900

    next12入門するぞ

---
+++ next12-app/pages/contact-page.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout"
+const Contact = () => {
+    return (
+      <Layout title="Contact">Contact page</Layout>
+    )
+}
+
+  export default Contact
---
+++ next12-app/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className='text-4xl'>Hello Nextjs</p>
+    </Layout>
+  )
+}
