0e45130c7e4a7f94df75062702f1a9e89971283b
yuki-tsurut <nfcgt.30@gmail.com>
2025-01-06 03:32:38 +0900

    記事の詳細ページ作成

--- next12-app/lib/posts.js
+++ next12-app/lib/posts.js
@@ -13,3 +13,39 @@ export async function getAllPostsData() {
         return [];
     }
 }
+
+export async function getAllPostIds() {
+    try {
+        const res = await fetch(new URL(apiUrl));
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const posts = await res.json();
+        return posts.map((post) => {
+            return {
+                params: {
+                    id: String(post.id),
+                },
+            };
+        });
+    } catch (error) {
+        console.error('データの取得に失敗しました:', error);
+        return [];
+    }
+}
+
+export async function getPostData(id) {
+    try {
+        const res = await fetch(new URL(`${apiUrl}/${id}`));
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const post = await res.json();
+        return {
+            post,
+        };
+    } catch (error) {
+        console.error('データの取得に失敗しました:', error);
+        return null;
+    }
+}
