7246e5f381d0c0eb87b750e53f442f444822b145
yuki-tsurut <nfcgt.30@gmail.com>
2025-01-06 21:36:09 +0900

    test

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("Hello React")
+  const [message, setMessage] = useState("Hello React");
   const programmingLanguages = ["JavaScript", "Python", "Ruby"];
+
+  const handleClick = () => setMessage("React is Fun");
 
   return (
     <div className="app-container">
-      <h1 className="title">{message}</h1><button onClick={() => setMessage("React is Fun")}>Change Message</button>
+      <h1 className="title">{message}</h1>
+      <button onClick={handleClick}>Change Message</button>
       <ul className="language-list">
-        {programmingLanguages.map((language, index) => (<li key={index} className="language-item">{language}</li>))}
+        {programmingLanguages.map((language) => (
+          <li key={language} className="language-item">
+            {language}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default App;
+
+ 
